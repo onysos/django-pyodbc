30612eb691c8b06788beb0ee7637b94b02807db4
darius BERNARD <darius.bernard@onysos.fr>
2014-01-10 12:14:37 +0100

    correction erreur de migration. added attribute «Database» for DatabaseWrapper for django1.6

--- sql_server/pyodbc/base.py
+++ sql_server/pyodbc/base.py
@@ -118,7 +118,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
 
     def __init__(self, *args, **kwargs):
         super(DatabaseWrapper, self).__init__(*args, **kwargs)
-
+        self.Database = Database
         options = self.settings_dict.get('OPTIONS', None)
 
         if options:
